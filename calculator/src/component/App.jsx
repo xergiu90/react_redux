67506d37d3b7e6772af67975da4537f298e1f174
Sergiu Manolache <smanolache@optaros.com>
2018-10-01 10:49:05 +0300

    Removed

--- calculator/src/component/App.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import Calculator from '../container/Calculator';
-
-export default class App extends React.Component {
-    render() {
-        return <Calculator />;
-    }
-};
